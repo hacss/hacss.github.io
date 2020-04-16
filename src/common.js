4b32f8b7e8dbf1d4af64e18c993bacfa8df2c731
Nick Saunders <nick@saunde.rs>
2020-04-15 17:04:27 -0700

    Add GitHub link

--- src/common.js
+++ src/common.js
@@ -35,22 +35,37 @@ exports.colors = {
 const navLinks = [
   {
     href: "getting-started.html",
-    title: "Getting Started",
+    content: "Getting Started",
     active: x => path.basename(x) === "getting-started.md",
   },
   {
     href: "guides.html",
-    title: "Guides",
+    content: "Guides",
     active: x => x.includes("guide"),
   },
   {
     href: "faq.html",
-    title: "FAQ",
+    content: "FAQ",
     active: x => path.basename(x) === "faq.md",
   },
   {
     href: "http://basement.hacss.io/",
-    title: "Basement",
+    content: "Basement",
+    active: () => false,
+  },
+  {
+    href: "https://github.com/hacss/hacss",
+    content: `
+      <svg viewBox="0 0 16 16" version="1.1" class="W(16px)">
+        <path fill="currentColor" fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+      </svg>
+    `,
+    popContent: `
+      <svg viewBox="0 0 16 16" version="1.1" class="W(16px)">
+        <path fill="currentColor" fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+      </svg>
+      <span class="D(ib) Mstart(8px)">GitHub</span>
+    `,
     active: () => false,
   },
 ];
@@ -98,23 +113,23 @@ exports.page = ({ title, content, source }) => `
               <rect width="100%" height="5px" y="16" />
             </svg>
           </button>
-          <nav class="Ff(ss) Fz(16px) C(blue200) D(if) D(n)--sm">
+          <nav class="Ff(ss) Fz(16px) C(blue200) D(if) Ai(c) D(n)--sm">
             ${
               navLinks
-                .map(({ href, title, active }) =>
+                .map(({ href, content, active }) =>
                   active(source)
                   ? `
                       <a
                         href="${href}"
-                        class="D(ib) Mstart(16px) C(white) Td(n)">
-                        ${title}
+                        class="D(if) Mstart(16px) C(white) Td(n)">
+                        ${content}
                       </a>
                     `
                   : `
                       <a
                         href="${href}"
-                        class="D(ib) Mstart(16px) C(blue300) C(white):h Td(n)">
-                        ${title}
+                        class="D(if) Mstart(16px) C(blue300) C(white):h Td(n)">
+                        ${content}
                       </a>
                     `
                 )
@@ -156,20 +171,20 @@ exports.page = ({ title, content, source }) => `
         ">
         ${
           navLinks
-            .map(({ href, title, active }) =>
+            .map(({ href, popContent, content, active }) =>
               active(source)
               ? `
                   <a
                     href="${href}"
-                    class="D(b) Px(8px) Py(4px) Bgc(purple200) C(purple900) Td(n)">
-                    ${title}
+                    class="D(f) Ai(c) Px(8px) Py(4px) Mih(28px) Bxz(bb) Bgc(purple200) C(purple900) Td(n)">
+                    ${popContent || content}
                   </a>
                 `
               : `
                   <a
                     href="${href}"
-                    class="D(b) Px(8px) Py(4px) C(purple600) Td(n) Bgc(purple600):h C(purple100):h">
-                    ${title}
+                    class="D(f) Ai(c) Px(8px) Py(4px) Mih(28px) Bxz(bb) C(purple600) Td(n) Bgc(purple600):h C(purple100):h">
+                    ${popContent || content}
                   </a>
                 `
             )
