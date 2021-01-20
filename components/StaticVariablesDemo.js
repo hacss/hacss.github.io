bb609439440eaea37e25d1fbbb8eac86d2a68f0a
Nick Saunders <nick@saunde.rs>
2021-01-19 23:29:50 -0700

    Reusable demo container

--- components/StaticVariablesDemo.js
+++ components/StaticVariablesDemo.js
@@ -1,13 +1,8 @@
 import { useEffect, useState } from "react";
+import HomePageDemo from "./HomePageDemo";
 import highlight from "../utils/highlight";
 
 export default function StaticVariablesDemo() {
-  return (
-    <FontSizeDemo />
-  );
-}
-
-function FontSizeDemo() {
   // font:$h1; font:$h2; font:$h3; font:$h4; font:$h5; font:$h6;
 
   const initial = "$h1";
@@ -35,56 +30,22 @@ function FontSizeDemo() {
   }, [codeArea, setCodeArea, current]);
 
   return (
-    <div className={`
-      display:flex;
-      flex-direction:column;
-      @lg{flex-direction:row}
-      border-radius:$md;
-      overflow:hidden;
-      height:$len192;
-      @lg{height:$len96}
-    `}>
-      <pre className={`
-        margin:0;
-        font:inherit;
-        box-shadow:$inner;
-        background:$gray90;
-        padding:$len16;
-        flex:1;
-        display:flex;
-        align-items:center;
-      `}>
-        <code
-          ref={setCodeArea}
-          className="font:$code; color:$gray30;"
-          dangerouslySetInnerHTML={{
-            __html: highlight(initHTML).replace(
-              initial,
-              `<span class="
-                  target
-                  .flash{background:$orange10}
-                  .flash{box-shadow:0__0__0__#{$len2}__#{$orange10}}
-                  .flash{color:$gray95}
-                  :not(.flash){transition-property:background,box-shadow,color}
-                  :not(.flash){transition-duration:1s}
-                ">${initial}</span>`
-            )
-          }} />
-      </pre>
-      <div
-        className={`
-          @lg{box-shadow:$inner}
-          background:$gray10;
-          color:$gray70;
-          flex:1;
-          display:flex;
-          align-items:center;
-          justify-content:center;
-          white-space:nowrap;
-          overflow:hidden;
-        `}
-        dangerouslySetInnerHTML={{ __html: initHTML.replace(initial, current) }} />
-    </div>
+    <HomePageDemo
+      left={{
+        __html: highlight(initHTML).replace(
+          initial,
+          `<span class="
+              target
+              .flash{background:$orange10}
+              .flash{box-shadow:0__0__0__#{$len2}__#{$orange10}}
+              .flash{color:$gray95}
+              :not(.flash){transition-property:background,box-shadow,color}
+              :not(.flash){transition-duration:1s}
+            ">${initial}</span>`
+        )
+      }}
+      leftRef={setCodeArea}
+      right={{ __html: initHTML.replace(initial, current) }} />
   );
 }
 
