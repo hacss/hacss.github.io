71ef80441bb397c847ebf1821489d347463b7518
Nick Saunders <nick@saunde.rs>
2021-01-24 08:21:40 -0700

    HomePageDemo fixed dimensions

--- components/HomePageDemo.js
+++ components/HomePageDemo.js
@@ -1,22 +1,29 @@
 export default function HomePageDemo({ left, leftRef, right }) {
   return (
     <div className={`
-      display:flex;
-      flex-direction:column;
-      @lg{flex-direction:row}
+      width:100%;
+      height:$len256;
+      @lg{height:$len128}
       border-radius:$md;
       overflow:hidden;
+      position:relative;
     `}>
       <pre className={`
+        position:absolute;
+        top:0;
+        right:0;
+        left:0;
+        height:$len128;
+        box-sizing:border-box;
+        @lg{right:50%}
         margin:0;
         font:inherit;
         box-shadow:$inner;
         background:$gray90;
         padding:$len16;
-        flex:1;
+        overflow:auto;
         display:flex;
         align-items:center;
-        overflow:auto;
       `}>
         <code
           ref={leftRef}
@@ -25,11 +32,16 @@ export default function HomePageDemo({ left, leftRef, right }) {
       </pre>
       <div
         className={`
+          position:absolute;
+          right:0;
+          bottom:0;
+          left:0;
+          height:$len128;
+          @lg{left:50%}
           @lg{box-shadow:$inner}
           background:$gray10;
           color:$gray70;
           font:$body1;
-          flex:1;
           display:flex;
           align-items:center;
           justify-content:center;
