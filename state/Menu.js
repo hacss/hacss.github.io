8ce62e79944491041e1049577c57c1e813141efb
Nick Saunders <nick@saunde.rs>
2021-01-31 17:28:55 -0700

    Auto collapse sidebar

--- state/Menu.js
+++ state/Menu.js
@@ -1,10 +1,29 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const Context = createContext([false, () => {}]);
 
-export const MenuState = props => (
-  <Context.Provider value={useState(true)} {...props} />
-);
+export const MenuState = props => {
+  const [open, setOpen] = useState(true);
+
+  useEffect(() => {
+    const collapse = () => {
+      const { matches } = matchMedia("(max-width:37.49999em)");
+      if (matches) {
+        setOpen(false);
+      }
+      else {
+        setOpen(true);
+      }
+    };
+    collapse();
+    window.addEventListener("resize", collapse);
+    return () => { window.removeEventListener("resize", collapse); };
+  }, []);
+
+  return (
+    <Context.Provider value={[open, setOpen]} {...props} />
+  );
+};
 
 export function useMenuState() {
   return useContext(Context);
