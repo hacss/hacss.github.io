87da9dc37e4ce7d87f2c02aa12684a5911e17ee8
Nick Saunders <nick@saunde.rs>
2021-02-04 07:10:53 -0700

    Markdown components wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ require( "@hacss/build");
 import { FC, useEffect, useState } from "react";
 import { AppProps } from "next/app";
 import { useRouter } from "next/router";
+import { ComponentType, MDXProvider } from "@mdx-js/react";
 import DocsApp from "./docs/_app";
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
@@ -17,19 +18,21 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
 
   return (
     <div className="position:absolute; inset:0;">
-      {
-        pathname.startsWith("/docs")
-        ? (
-            <DocsApp
-              pathname={pathname.substring(5)}
-              sidebarState={sidebarState}
-              onSidebarOpen={openSidebar}
-              onSidebarClose={closeSidebar}>
-              <Component {...pageProps} />
-            </DocsApp>
-          )
-        : (<Component {...pageProps} />)
-      }
+      <MDXProvider components={markdownComponents}>
+        {
+          pathname.startsWith("/docs")
+          ? (
+              <DocsApp
+                pathname={pathname.substring(5)}
+                sidebarState={sidebarState}
+                onSidebarOpen={openSidebar}
+                onSidebarClose={closeSidebar}>
+                <Component {...pageProps} />
+              </DocsApp>
+            )
+          : (<Component {...pageProps} />)
+        }
+      </MDXProvider>
     </div>
   );
 }
@@ -59,4 +62,33 @@ const useSidebarState = (): ["open" | "closed", () => void, () => void] => {
     () => setOpen(true),
     () => setOpen(false)
   ];
+};
+
+type UnionToIntersection<U> = 
+  (U extends any ? (k: U)=>void : never) extends ((k: infer I)=>void) ? I : never;
+
+type MarkdownComponents<K = ComponentType> = UnionToIntersection<K extends ComponentType ? Record<K, FC<any>> : {}>;
+
+const markdownComponents: Partial<MarkdownComponents> = {
+  h1: ({ children }) => (
+    <h1 className="font:$h1; margin-y:$len8;">{children}</h1>
+  ),
+  h2: ({ children }) => (
+    <h2 className="font:$h2; margin-y:$len8;">{children}</h2>
+  ),
+  h3: ({ children }) => (
+    <h3 className="font:$h3; margin-y:$len8;">{children}</h3>
+  ),
+  a: ({ children, className, ...props }) => (
+    <a
+      className={`
+        ${className || ""}
+        color:$blue80;
+        :hover{color:$blue60}
+        :active{color:$red60}!
+      `}
+      {...props}>
+      {children}
+    </a>
+  )
 };
