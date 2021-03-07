d637e92e6844820ee60002723f48b43111a13ffc
Nick Saunders <nick@saunde.rs>
2021-03-07 11:15:37 -0700

    Add syntax article.

--- pages/docs/_app.tsx
+++ pages/docs/_app.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 const pages = [
   ["", "Getting Started"],
   ["/fundamentals", "Fundamentals"],
+  ["/syntax", "Syntax"],
   ["/configuration", "Configuration"],
   ["/cli", "Command-line interface"],
   ["/webpack", "Webpack"],
